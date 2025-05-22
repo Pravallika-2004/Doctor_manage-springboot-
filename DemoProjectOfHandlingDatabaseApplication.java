25632348b8820bad3baba7b831ec98582bb4b0fc
Kurmala Pravallika <139159951+Pravallika-2004@users.noreply.github.com>
2025-05-22 12:56:08 +0530

    Add files via upload

---
+++ DemoProjectOfHandlingDatabaseApplication.java
@@ -0,0 +1,14 @@
+package com.handlingdatabase.demo.project.of.handling.database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProjectOfHandlingDatabaseApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DemoProjectOfHandlingDatabaseApplication.class, args);
+	}
+
+}
